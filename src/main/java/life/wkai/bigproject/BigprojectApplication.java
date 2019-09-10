0b1a5930142da6f74a94ef553dc37f8916a45516
wkai <911424930@qq.com>
2019-09-10 15:35:23 +0800

    init commit

---
+++ src/main/java/life/wkai/bigproject/BigprojectApplication.java
@@ -0,0 +1,13 @@
+package life.wkai.bigproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigprojectApplication.class, args);
+    }
+
+}
